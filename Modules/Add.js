de04244a0fa2ea08d1d437c27285dd2d7e26114a
the-samarium <tropicalvoilet008@gmail.com>
2025-03-29 22:09:56 +0530

    Add folder

---
+++ Modules/Add.js
@@ -0,0 +1,14 @@
+ function add(a,b){
+    console.log(a+b)
+}
+ function mul(a,b){
+    console.log(a*b)
+}
+function div(a,b){
+    console.log(a/b)
+}
+function sub(a,b){
+    console.log(a-b)
+}
+
+// export{mul,add,sub,div}
---
+++ Modules/maths.js
@@ -0,0 +1,6 @@
+import * as M from "./Add.js"
+
+M.add(2,3)
+M.mul(10,3)
+M.sub(10,23)
+M.div(70,7)
